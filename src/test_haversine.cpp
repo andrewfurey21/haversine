1905a7bb23fc8056d717c2361bbfeeece581f953
andrewfurey21 <andrewfurey2003@gmail.com>
2026-04-04 22:15:12 -0700

    some basic profiling

--- src/test_haversine.cpp
+++ src/test_haversine.cpp
@@ -4,6 +4,11 @@
 #include <cmath>
 #include <cstdio>
 
+#include <sys/time.h>
+#include <x86intrin.h>
+
+static inline u64 read_cpu_timer() { return __rdtsc(); }
+
 int main(int argc, char ** argv) {
   if (argc != 3) {
     printf("Usage: ./test <json> <outputs>\n");
@@ -16,20 +21,28 @@ int main(int argc, char ** argv) {
   const char * outputs_filename = argv[2];
 
   FILE * json_file = NULL, * outputs_file = NULL;
+
+  u64 open_json_file_start = read_cpu_timer();
   json_file = fopen(json_filename, "r");
   if (json_file == NULL) {
     printf("Could not open json_file\n");
     return 1;
   }
+  u64 open_json_file_end = read_cpu_timer();
+
+  u64 open_answer_file_start = read_cpu_timer();
   outputs_file = fopen(outputs_filename, "rb");
   if (outputs_file == NULL) {
     printf("Could not open outputs file\n");
     return 1;
   }
+  u64 open_answer_file_end = read_cpu_timer();
 
-
+  u64 parse_json_start = read_cpu_timer();
   JSONElement * json  = parse_json(json_file);
+  u64 parse_json_end = read_cpu_timer();
 
+  u64 calculate_haversine_start = read_cpu_timer();
   JSONElement * pairs = get_object_value(json, "pairs");
 
   JSONElement * current_pair = pairs;
@@ -58,11 +71,28 @@ int main(int argc, char ** argv) {
     current_pair = current_pair->next;
     counter++;
   }
+  u64 calculate_haversine_end = read_cpu_timer();
 
+  u64 destroy_json_start = read_cpu_timer();
   destroy_json(json);
   fclose(outputs_file);
   fclose(json_file);
+  u64 destroy_json_end = read_cpu_timer();
+
 
   printf("Success.\n");
+  u64 open_json_file_clocks = open_json_file_end - open_json_file_start;
+  u64 open_answer_file_clocks = open_answer_file_end - open_answer_file_end;
+  u64 parse_json_clocks = parse_json_end - parse_json_start;
+  u64 calculate_haversine_clocks = calculate_haversine_end - calculate_haversine_start;
+  u64 destroy_json_clocks = destroy_json_end - destroy_json_start;
+
+  u64 total_clocks = open_json_file_clocks + open_answer_file_clocks + parse_json_clocks + calculate_haversine_clocks + destroy_json_clocks;
+
+  printf("Open json file: %f\n", 100.0 * (f64)open_json_file_clocks / total_clocks);
+  printf("Open answer file: %f\n", 100.0 * (f64)open_answer_file_clocks / total_clocks);
+  printf("Parse json file: %f\n", 100.0 * (f64)parse_json_clocks / total_clocks);
+  printf("Calculate haversine: %f\n", 100.0 * (f64)calculate_haversine_clocks / total_clocks);
+  printf("Destroy json: %f\n", 100.0 * (f64)destroy_json_clocks / total_clocks);
   return 0;
 }
---
+++ timer.cpp
@@ -0,0 +1,50 @@
+#include <cstdint>
+#include <cstdio>
+
+#include <x86intrin.h>
+#include <sys/time.h>
+
+using u64 = uint64_t;
+using f64 = double;
+
+static u64 get_os_timer_freq() {
+  return 1'000'000; // os timer is in microseconds.
+}
+
+static u64 read_os_timer() {
+  struct timeval value;
+  gettimeofday(&value, 0);
+
+  return get_os_timer_freq() * value.tv_sec + value.tv_usec;
+}
+
+static inline u64 read_cpu_timer() { return __rdtsc(); }
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    printf("Usage: ./timer <microseconds>");
+    return 1;
+  }
+
+  u64 wait_time = atol(argv[1]);
+  u64 cpu_clocks_start = read_cpu_timer();
+
+  u64 start = read_os_timer();
+  u64 elapsed = 0;
+
+  while (elapsed < wait_time) {
+    u64 current = read_os_timer();
+    elapsed = current - start;
+  }
+
+  u64 cpu_clocks_end = read_cpu_timer();
+
+  u64 cpu_clocks = cpu_clocks_end - cpu_clocks_start;
+  printf("Finsihed. %lu, %lu\n", elapsed, cpu_clocks);
+
+  // TODO: profile parts of the haversine processor.
+
+  u64 clock_rate = (f64)cpu_clocks * get_os_timer_freq() / elapsed;
+  printf("Estimated clock rate: %lu\n", clock_rate);
+}
+
